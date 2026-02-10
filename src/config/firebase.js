a4177f53118e713ff25a7b25526c3826cc42e606
Jazz <jaswinderknanda@outlook.com>
2026-02-09 21:03:48 -0700

    At 9:03 pm

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,30 @@
 var admin = require("firebase-admin");
+dotenv = require("dotenv").config();
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key.replace(/\\n/g, "\n"),
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: process.env.universe_domain
+  }),
+
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
 
 var serviceAccount = require("./simple-restapi-32f83-firebase-adminsdk.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://simple-restapi-32f83-default-rtdb.firebaseio.com"
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://simple-restapi-32f83-default-rtdb.firebaseio.com"
+// });
 
 const db = admin.database();
 module.exports = db;
@@ -59,35 +78,4 @@ module.exports = db;
 
 
 
-// //   import { initializeApp } from "firebase/app";
-// //   import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
-//   require("dotenv").config();
-  
-
-  
-
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// // const firebaseConfig = {
-// //   apiKey: "AIzaSyBuo-RuCDQOlJtfbKU4nM3rmty27HsOUu4",
-// //   authDomain: "simple-restapi-32f83.firebaseapp.com",
-// //   databaseURL: "https://simple-restapi-32f83-default-rtdb.firebaseio.com",
-// //   projectId: "simple-restapi-32f83",
-// //   storageBucket: "simple-restapi-32f83.firebasestorage.app",
-// //   messagingSenderId: "364619942130",
-// //   appId: "1:364619942130:web:d92d3fa6f70182eed4686b",
-// //   measurementId: "G-FQ5MR3QP1D"
-// // };
-
-// //  const app = initializeApp(firebaseConfig);
-// //  const db = getDatabase(app);
-
-// /* Initialize Firebase Admin SDK */
-// var admin = require("firebase-admin");
-
-// var serviceAccount = require("./simple-restapi.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://simple-restapi-32f83-default-rtdb.firebaseio.com"
-// });
 
